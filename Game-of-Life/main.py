d0af56ff0b93d095b980a94d2b555625d8475af1
CarCedo <CarCedo.Pro@gmail.com>
2023-07-31 01:55:52 +0330

    Setup the GOL instance

--- Game-of-Life/main.py
+++ Game-of-Life/main.py
@@ -1,19 +1,29 @@
+import pygame
+
+from colors import Colors
 from engine import Engine
 
 
 class GameOfLife(Engine):
     def init_game(self):
-        pass
+        self.cell_size = self.configs["cell_size"]
 
     def check_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.display.fill(Colors.RAYWHITE)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
-    GameOfLife(configs={})
+    GameOfLife(
+        configs={
+            "cell_size": (32, 32),
+        }
+    )
