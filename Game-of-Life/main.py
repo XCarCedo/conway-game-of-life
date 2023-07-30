cf3570e3a1e83f2aeb0cdc426f2aee3a1ae6e3b0
CarCedo <CarCedo.Pro@gmail.com>
2023-07-31 02:17:57 +0330

    Created board in GOL instance and added Cell classes with status

--- Game-of-Life/main.py
+++ Game-of-Life/main.py
@@ -2,11 +2,34 @@ import pygame
 
 from colors import Colors
 from engine import Engine
+from enum import Enum
+
+
+class CellStatus(Enum):
+    ALIVE = 1
+    DEAD = 0
+
+
+class Cell:
+    def __init__(self, status: CellStatus):
+        self.status = status
+
+    def __repr__(self):
+        return f"Cell({self.status})"
 
 
 class GameOfLife(Engine):
     def init_game(self):
         self.cell_size = self.configs["cell_size"]
+        self.board = []
+
+        self.init_board()
+
+    def init_board(self):
+        for x in range(self.screen_size[0] // self.cell_size[0]):
+            self.board.append([])
+            for y in range(self.screen_size[1] // self.cell_size[1]):
+                self.board[x].append(Cell(CellStatus.DEAD))
 
     def check_events(self):
         for event in pygame.event.get():
@@ -14,16 +37,20 @@ class GameOfLife(Engine):
                 self.running = False
 
     def update(self):
-        pass
+        pygame.display.set_caption(str(self.clock.get_fps()))
 
     def draw(self):
         self.display.fill(Colors.RAYWHITE)
         pygame.display.flip()
 
+    def draw_cells(self):
+        pass
+
 
 if __name__ == "__main__":
     GameOfLife(
+        screen_size=(640, 640),
         configs={
             "cell_size": (32, 32),
-        }
+        },
     )
