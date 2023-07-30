6cf073de73cf2575f2dd86f1f56b04bef1642f34
CarCedo <CarCedo.Pro@gmail.com>
2023-07-31 02:35:01 +0330

    Added drawing method for cell and the board without interactive functionality for now

--- Game-of-Life/main.py
+++ Game-of-Life/main.py
@@ -11,7 +11,9 @@ class CellStatus(Enum):
 
 
 class Cell:
-    def __init__(self, status: CellStatus):
+    def __init__(self, cell_x: int, cell_y: int, status: CellStatus):
+        self.cell_x = cell_x
+        self.cell_y = cell_y
         self.status = status
 
     def __repr__(self):
@@ -28,8 +30,8 @@ class GameOfLife(Engine):
     def init_board(self):
         for x in range(self.screen_size[0] // self.cell_size[0]):
             self.board.append([])
-            for _ in range(self.screen_size[1] // self.cell_size[1]):
-                self.board[x].append(Cell(CellStatus.DEAD))
+            for y in range(self.screen_size[1] // self.cell_size[1]):
+                self.board[x].append(Cell(x, y, CellStatus.DEAD))
 
     def check_events(self):
         for event in pygame.event.get():
@@ -40,11 +42,32 @@ class GameOfLife(Engine):
         pygame.display.set_caption(str(self.clock.get_fps()))
 
     def draw(self):
-        self.display.fill(Colors.RAYWHITE)
+        self.display.fill(Colors.GRAY)
+
+        self.draw_cells()
+
         pygame.display.flip()
 
     def draw_cells(self):
-        pass
+        for row in self.board:
+            for cell in row:
+                position_on_screen = (
+                    cell.cell_x * self.cell_size[0],
+                    cell.cell_y * self.cell_size[1],
+                )
+
+                cell_color = (
+                    Colors.RAYWHITE if cell.status == CellStatus.ALIVE else Colors.BLACK
+                )
+                pygame.draw.rect(
+                    self.display,
+                    Colors.RAYWHITE,
+                    pygame.Rect(
+                        *position_on_screen,
+                        self.cell_size[0] - 1,
+                        self.cell_size[1] - 1,
+                    ),
+                )
 
 
 if __name__ == "__main__":
