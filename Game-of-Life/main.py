f363d4521b87baba9c7159b6bd3470d25639b7f2
CarCedo <CarCedo.Pro@gmail.com>
2023-07-31 07:43:50 +0330

    Completed cell editor fully functional, able to toggle cells by lmb button

--- Game-of-Life/main.py
+++ Game-of-Life/main.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 
 from colors import Colors
@@ -12,8 +13,14 @@ class CellStatus(Enum):
 
 class Cell:
     def __init__(
-        self, cell_x: int, cell_y: int, cell_size: tuple[int, int], status: CellStatus
+        self,
+        cell_x: int,
+        cell_y: int,
+        cell_size: tuple[int, int],
+        status: CellStatus,
+        game,
     ):
+        self.game = game
         self.cell_x = cell_x
         self.cell_y = cell_y
         self.status = status
@@ -23,6 +30,12 @@ class Cell:
             cell_size[0] - 1,
             cell_size[0] - 1,
         )
+
+    def toggle(self):
+        self.status = {
+            CellStatus.ALIVE: CellStatus.DEAD,
+            CellStatus.DEAD: CellStatus.ALIVE,
+        }[self.status]
 
     def __repr__(self):
         return f"Cell({self.status})"
@@ -39,15 +52,27 @@ class GameOfLife(Engine):
         for x in range(self.screen_size[0] // self.cell_size[0]):
             self.board.append([])
             for y in range(self.screen_size[1] // self.cell_size[1]):
-                self.board[x].append(Cell(x, y, self.cell_size, CellStatus.DEAD))
+                self.board[x].append(Cell(x, y, self.cell_size, CellStatus.DEAD, self))
 
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                lmb_pressed = event.button == 1
+                if not lmb_pressed:
+                    continue
+                mouse_pos = pygame.mouse.get_pos()
+                clicked_cell_pos = (
+                    math.floor(mouse_pos[0] / self.cell_size[0]),
+                    math.floor(mouse_pos[1] / self.cell_size[1]),
+                )
+
+                clicked_cell = self.board[clicked_cell_pos[0]][clicked_cell_pos[1]]
+                clicked_cell.toggle()
 
     def update(self):
-        pygame.display.set_caption(str(self.clock.get_fps()))
+        pass
 
     def draw(self):
         self.display.fill(Colors.GRAY)
