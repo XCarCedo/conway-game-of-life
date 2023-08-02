a10eba8a0c1d18bf8fbe29426988a1198483beeb
CarCedo <CarCedo.Pro@gmail.com>
2023-08-02 07:49:29 +0330

    Documented code using doc-string and added type annotation

--- Game-of-Life/engine.py
+++ Game-of-Life/engine.py
@@ -5,6 +5,19 @@ from abc import abstractmethod
 
 
 class Engine(ABC):
+
+    """The abstract engine class that implement basic features like game loop, fps locker and etc...
+
+    Attributes:
+        clock (pygame.time.Clock): The clock object used to lock fps
+        configs (dict): The configs dictionary is saved as self.configs for the game class that inherits Engine
+        display (pygame.Surface): The main window display surface
+        fps (int): The locked fps value
+        last_tick_time (int): Processing time used in the last tick, can be used with timers
+        running (bool): Determines the application is running or not, self.running=False to close
+        screen_size (tuple[int,int]): The screen size
+    """
+
     def __init__(self, screen_size=(560, 560), fps=60, configs={}) -> None:
         self.screen_size = screen_size
         self.configs = configs
@@ -14,33 +27,44 @@ class Engine(ABC):
         self.init_engine()
         self.run()
 
-    def init_engine(self):
+    def init_engine(self) -> None:
+        """Initialize the engine and graphics libraries used"""
         pygame.init()
 
         self.running = True
         self.display = pygame.display.set_mode(self.screen_size)
         self.clock = pygame.time.Clock()
 
-    def _fps_locker(self):
+    def _fps_locker(self) -> None:
+        """Locks the fps to self.fps and saves the time passed into self.last_tick_time"""
         self.last_tick_time = self.clock.tick(self.fps)
 
     @abstractmethod
     def init_game(self):
+        """The game class that inherits can use this method to initialize the game
+        This method is only called one time at the beggining of game loop before everyting else
+        """
         pass
 
     @abstractmethod
     def check_events(self):
+        """The game class that inherits can use this method to check for actions and event updates"""
         pass
 
     @abstractmethod
     def update(self):
+        """The game class that inerhits can use this method to update the state of application
+        every frame
+        """
         pass
 
     @abstractmethod
     def draw(self):
+        """The game class that inherits can use this method to draw the updates to display surface"""
         pass
 
     def run(self):
+        """Run's the game loop"""
         self.init_game()
         while self.running:
             self._fps_locker()
--- Game-of-Life/main.py
+++ Game-of-Life/main.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import math
+from typing import Any, Union
 import pygame
 import tkinter
 import tkinter.filedialog
@@ -16,6 +17,20 @@ class CellStatus(Enum):
 
 
 class Cell:
+
+    """This cell class is used to store information about each one of cells and perform operations
+
+    Attributes:
+        cell_size (tuple[int, int]): The size of cell
+        cell_x (int): The x position of the cell (The position is relative to other cells
+        not to be messed with pixel position)
+        cell_y (int): The y position of the cell (The position is relative to other cells
+        not to be messed with pixel position
+        game (GameOfLife): The game class that inherits engine
+        rect (pygame.Rect): The pygame rectangle object used to store height,width,x,y
+        status (CellStatus): The status of current cell determined by CellStatus enum class
+    """
+
     def __init__(
         self,
         cell_x: int,
@@ -36,16 +51,27 @@ class Cell:
             cell_size[0] - 1,
         )
 
-    def toggle(self):
+    def toggle(self) -> None:
+        """Toggle the status of cell, if its alive it becomes dead and vice versa"""
         self.status = {
             CellStatus.ALIVE: CellStatus.DEAD,
             CellStatus.DEAD: CellStatus.ALIVE,
         }[self.status]
 
     def copy(self):
+        """Returns the copy of itself
+
+        Returns:
+            Cell: The copy of itself
+        """
         return Cell(self.cell_x, self.cell_y, self.cell_size, self.status, self.game)
 
     def get_neighbors_count(self) -> int:
+        """Calculates the amount of alive neighbors
+
+        Returns:
+            int: Teh amount of alive neighbors
+        """
         neighbor_count = 0
 
         # Top
@@ -72,7 +98,10 @@ class Cell:
 
 
 class GameOfLife(Engine):
-    def init_game(self):
+    def init_game(self) -> None:
+        """Initialize the game object and gets the configs, also if the program is a in a loaded state
+        it processes the state file to display in board
+        """
         self.cell_size = self.configs["cell_size"]
         self.evoulate_timer_ms = self.configs["evoulate_timer_ms"]
         self.since_last_evoulate = 0
@@ -85,13 +114,15 @@ class GameOfLife(Engine):
             self.board = []
             self.init_board()
 
-    def init_board(self):
+    def init_board(self) -> None:
+        """If the progarm is not in a loaded state creates each cell with defautl statues of DEAD"""
         for x in range(self.screen_size[0] // self.cell_size[0]):
             self.board.append([])
             for y in range(self.screen_size[1] // self.cell_size[1]):
                 self.board[x].append(Cell(x, y, self.cell_size, CellStatus.DEAD, self))
 
-    def check_events(self):
+    def check_events(self) -> None:
+        """Checks for space/c clicks to run/clear the board"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
@@ -104,7 +135,10 @@ class GameOfLife(Engine):
             ):
                 self.clear_board()
 
-    def update(self):
+    def update(self) -> None:
+        """Updates the game caption with new info, checks evoulate timer to run evoulate if needed and
+        if the program is in edit state allows the user to edit cells with mouse
+        """
         pygame.display.set_caption(
             f"Conway's Game Of Life | "
             + f"{'Evoulating' if self.running_evoulation else 'Paused'} | "
@@ -135,20 +169,35 @@ class GameOfLife(Engine):
                 elif pressed_keys[pygame.K_r]:
                     self.load_state_file()
 
-    def draw(self):
+    def draw(self) -> None:
+        """Draws the cells into screen"""
         self.display.fill(Colors.GRAY)
 
         self.draw_cells()
 
         pygame.display.flip()
 
-    def get_saveable_board(self):
+    def get_saveable_board(self) -> list[list[int]]:
+        """Returns a 2 Dimensional list of itself changing the cell classes to 1 and 0 so
+        it's valid to save
+
+        Returns:
+            list[list[int]]: The 2 dimensional list in 1,0 format
+        """
         return [
             [{CellStatus.DEAD: 0, CellStatus.ALIVE: 1}[cell.status] for cell in row]
             for row in self.board
         ]
 
-    def get_loadable_board(self, config_json):
+    def get_loadable_board(self, config_json: list[list[int]]) -> list[list[Cell]]:
+        """Returns a display-able board, changes all 1,0 to valid Cell objects
+
+        Args:
+            config_json (list[list[int]]): 2 dimensional list containing 1,0
+
+        Returns:
+            list[list[Cell]]: Converted 2 dimensional list 1,0 to valid Cell objects
+        """
         new_board = []
         for x, row in enumerate(config_json):
             new_board.append([])
@@ -164,7 +213,13 @@ class GameOfLife(Engine):
                 )
         return new_board
 
-    def unpack_state(self, config_json):
+    def unpack_state(self, config_json: dict[str, Union[int, list[list[int]]]]) -> None:
+        """Unpacks the loaded config_json file and passes it into the new GameOfLife object
+        used to load states
+
+        Args:
+            config_json (dict[Union[int, list[list[int]]]]): Description
+        """
         self.running = False
         GameOfLife(
             config_json["screen_size"],
@@ -176,7 +231,8 @@ class GameOfLife(Engine):
             },
         )
 
-    def save_state_file(self):
+    def save_state_file(self) -> None:
+        """Saves the state of application and its info into a file"""
         top = tkinter.Tk()
         top.withdraw()
         state_file_path = tkinter.filedialog.asksaveasfilename(
@@ -192,7 +248,10 @@ class GameOfLife(Engine):
         with open(state_file_path, mode="w") as save_state_file:
             json.dump(saved_state, save_state_file)
 
-    def load_state_file(self):
+    def load_state_file(self) -> None:
+        """Loades the saved state from file, unpacks it, runs a new GameOfLife object with loaded
+        board and configs
+        """
         top = tkinter.Tk()
         top.withdraw()
         state_file_path = tkinter.filedialog.askopenfilename(
@@ -204,12 +263,23 @@ class GameOfLife(Engine):
             saved_state = json.load(load_state_file)
             self.unpack_state(saved_state)
 
-    def clear_board(self):
+    def clear_board(self) -> None:
+        """Clears the baord and makes all cell's dead"""
         for row in self.board:
             for cell in row:
                 cell.status = CellStatus.DEAD
 
-    def get_cell_status(self, cell_x, cell_y):
+    def get_cell_status(self, cell_x: int, cell_y: int) -> int:
+        """Gets the status of requested cell and returns it as 1,0 (Also returns 0 if the requested
+        cell position is invalid)
+
+        Args:
+            cell_x (int): The x position of cell (this position is relative to other cells)
+            cell_y (int): The y position of cell (this position is relative to other cells)
+
+        Returns:
+            int: 1 if cell is alive 0 if its dead or position is invalid
+        """
         try:
             return {CellStatus.ALIVE: 1, CellStatus.DEAD: 0}[
                 self.board[cell_x][cell_y].status
@@ -217,7 +287,8 @@ class GameOfLife(Engine):
         except IndexError:
             return 0
 
-    def draw_cells(self):
+    def draw_cells(self) -> None:
+        """Draw's each one of the cells into screen with colors"""
         for row in self.board:
             for cell in row:
                 cell_color = {
@@ -231,7 +302,8 @@ class GameOfLife(Engine):
                     cell.rect,
                 )
 
-    def evoulate(self):
+    def evoulate(self) -> None:
+        """Evoulates the board into it next state, the main part of conway's game of life"""
         next_level_board = [[cell.copy() for cell in row] for row in self.board]
         for x, row in enumerate(self.board):
             for y, cell in enumerate(row):
@@ -244,6 +316,11 @@ class GameOfLife(Engine):
 
 
 def get_args_config() -> argparse.Namespace:
+    """Loads command-line arguments and returns it
+
+    Returns:
+        argparse.Namespace: The loaded arguments
+    """
     parser = argparse.ArgumentParser(description="Conway's Game of Life")
 
     parser.add_argument(
